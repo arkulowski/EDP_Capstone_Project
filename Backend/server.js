e5a4960cfe783fd5adfb6da485c3fef77b12d191
Chikwendu Mgbechi <chikwe2002@gmail.com>
2025-07-10 13:57:49 -0400

    search route implemented

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,7 @@ MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     console.error('Error connecting to MongoDB:', err);
   });
 
-  // Route: /
+// Route: /
 app.get('/', async (req, res) => {
     try {
         res.status(200).send('Server is running');
@@ -35,10 +35,66 @@ app.get('/', async (req, res) => {
       }
 });
 
-//test endpoint
+// Route: /api/ping
 app.get('/api/ping', (req, res) => {
   res.json({ message: 'ping from the server'})
 
+})
+
+// Route: /api/employees
+app.get('/api/employees', async (req, res) => {
+  try {
+    // Logic to fetch employees
+    const employeeCollection = db.collection('Employees');
+    const employees = await employeeCollection.find({}).toArray();
+    res.json(employees);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error fetching employees" });
+  }
+});
+
+// Route: /api/employees/:id
+app.get('/api/employees/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    // Logic to fetch a specific employee by id
+    const employeeCollection = db.collection('Employees');
+    const employee = await employeeCollection.findOne({employee_id : parseInt(id)});
+    res.json(employee);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: `Error fetching character with ID: ${id}` });
+  }
+});
+
+// Route: /api/search
+app.get('/api/search', async (req, res) =>{
+  const { fQuery, lQuery } = req.query
+  const query = [
+   
+  ] 
+
+  if (fQuery){
+    query.push({firstname: { $regex: `^${fQuery}`, $options: 'i' }})
+  }
+
+  if (lQuery) {
+    query.push({lastname: { $regex: `^${lQuery}`, $options: 'i' }})
+  }
+
+  try {
+    // Logic to return searched values
+    const employeeCollection = db.collection('Employees');
+    const employee = await employeeCollection.find({
+      $and: query
+    }).toArray();
+    res.json(employee);
+    
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Error searching employees' });
+  }
 })
 
 app.listen(PORT, () => {
